a9f6215dda88b3f87979ab6b5c25fb0b6831e9c3
Adrien Canterot <adrien@MacBook-Pro-de-Adrien.local>
2014-10-22 12:09:23 +0200

    Initial Commit

---
+++ CubeDodger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CubeDodger
+//
+//  Created by Adrien Canterot on 22/10/2014.
+//  Copyright (c) 2014 Adrien Canterot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
